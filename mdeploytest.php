1223fb2c14f151ecfd8b59accc6f2e2b4dba6a8e
joan <joan@joan-lg>
2012-12-21 10:56:11 +0100

    4th  commit git local another branch

--- mdeploytest.php
+++ mdeploytest.php
@@ -14,7 +14,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
+// anohther test of git
 /**
  * PHPUnit tests for the mdeploy.php utility
  *
