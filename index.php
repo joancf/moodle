b8a1adafcf8e5f37324a8d8fbb3f2657c3b0b2fb
joan <joan@joan-lg>
2012-12-21 10:29:46 +0100

    2nd commit git local

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+// testing git second commit local
 /**
  * Moodle frontpage.
  *
